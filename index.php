72ad8caa45ad06e0795cd3c0f4b9eb27069c0926
Ruma <matteoh8@hotmail.it>
2022-12-13 14:33:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <?php
+        $search = $_GET['word'];
+        $newArray = '';
+    ?>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, incidunt id maiores et at quibusdam deserunt alias sequi delectus itaque porro, minima ipsam! Mollitia similique dolore dolorem. Suscipit, voluptates optio.</p>
+    <form action="" method="GET">
+        <label for="word">Parola da censurare</label>
+        <input type="text" id="word" name="search">
+        <button>Cenzura!</button>
+    </form>
+    <h1>Nuovo testo censurato</h1>
+    <?= "<p>${newArray}</p>"; ?>
+</body>
+</html>
